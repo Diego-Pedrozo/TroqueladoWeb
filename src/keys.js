d09831fcb27821b5568d845aed36394a1784a274
Diego-Pedrozo <diegoarmandopvil@ufps.edu.co>
2022-10-17 11:58:55 -0500

    Ajustes de conexion

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     database: {
         host: 'us-cdbr-east-06.cleardb.net',
-        user: 'b4fefa2abeb128:f9bb601b',
+        user: 'b4fefa2abeb128',
         password: 'f9bb601b',
         database: 'heroku_5a82b2f600dc2ce'
     }
