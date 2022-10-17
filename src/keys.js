99260026e14f8c4793d0a449d2840784c942a0fd
Diego-Pedrozo <diegoarmandopvil@ufps.edu.co>
2022-10-17 02:10:10 -0500

    AjusteDB

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'troquelado'
+        host: 'us-cdbr-east-06.cleardb.net',
+        user: 'b4fefa2abeb128:f9bb601b',
+        password: 'f9bb601b',
+        database: 'heroku_5a82b2f600dc2ce'
     }
 }
