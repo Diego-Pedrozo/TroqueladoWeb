8fa0ee8c07c220699599025ebc7c35ed8c4101a6
Diego-Pedrozo <diegoarmandopvil@ufps.edu.co>
2022-10-17 14:00:37 -0500

    Index reparado

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const pool = require('../database');
 const { createPool } = require('mysql');
 
-router.get('/', (req, res) => {
-     res.render('index');
-});
+// router.get('/', (req, res) => {
+//      res.render('index');
+// });
 
-//  router.get('/',  async (req, res) => {
-//      const registros = await pool.query('SELECT * FROM registro')
-//      res.render('index', { registros });
-//  });
+router.get('/',  async (req, res) => {
+    const registros = await pool.query('SELECT * FROM registro')
+    res.render('index', { registros });
+ });
 
 module.exports = router;
